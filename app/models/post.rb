bf6723539f9c0d9f67f63bedbdd8a74fb07be5f6
Nihar Patel <niharpatel52@gmail.com>
2025-08-19 11:48:36 -0400

    Add TitleValidator Class and create custom calickbail validator

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+  validates_with TitleValidator
 end
--- app/validators/title_validator.rb
+++ app/validators/title_validator.rb
@@ -1,7 +1,9 @@
 class TitleValidator < ActiveModel::Validator
-  def validate(title)
-    unless title.email.match?(/flatironschool.com/)
-      record.errors[:email] << "We're only allowed to have people who work for the company in the database!"
+  def validate(record)
+    if record.title.present?
+      unless record.title.match?(/Won't Believe|Secret|Top [0-9]+|Guess/)
+        record.errors.add(:title, "not clickbait")
+      end
     end
   end
 end
